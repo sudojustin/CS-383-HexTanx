adc52c464741ca654632ba5b1b8461a6af80520f
Brendomcwumbo <148289995+Brendomcwumbo@users.noreply.github.com>
2025-04-15 13:03:46 -0700

    Commit

--- Assets/src/TL3_Brenden/Scripts (Future)/TankManager.cs
+++ Assets/src/TL3_Brenden/Scripts (Future)/TankManager.cs
@@ -9,6 +9,12 @@ public class TankManager : MonoBehaviour
     public GameObject level4TankPrefab;
     public GameObject level4377TankPrefab;
     public GameObject levelEasterTankPrefab;
+    public GameObject level5TankPrefab;
+    public GameObject level6TankPrefab;
+    public GameObject level7TankPrefab;
+    public GameObject level8TankPrefab;
+    public GameObject level9TankPrefab;
+    public GameObject level10TankPrefab;
 
     public GameObject GetTankForCurrentScene()
     {
@@ -28,6 +34,18 @@ public class TankManager : MonoBehaviour
                 return level4377TankPrefab;
             case "EasterLevel":
                 return levelEasterTankPrefab;
+            case "Level5":
+                return level5TankPrefab;
+            case "Level6":
+                return level6TankPrefab;
+            case "Level7":
+                return level7TankPrefab;
+            case "Level8":
+                return level8TankPrefab;
+            case "Level9":
+                return level9TankPrefab; ;
+            case "Level10":
+                return level10TankPrefab;
             default:
                 //Debug.LogWarning("TankManager: Unknown scene name, defaulting to Level1 tank.");
                 return level1TankPrefab;
