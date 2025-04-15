bacb774ea561634d6ce373337b4dcc2c8dc93f6d
Brendomcwumbo <148289995+Brendomcwumbo@users.noreply.github.com>
2025-04-15 14:44:52 -0700

    bvljhvmhc ngc,jknb.k,hvkhjmfcmhj,nmb,jvb,mnb,mnb,jhfcvmhgv ,kjgkjgcv,kjhbkjgxckhgvkmfxcmgnbvb

--- Assets/src/TL3_Brenden/Scripts (Future)/TankType.cs
+++ Assets/src/TL3_Brenden/Scripts (Future)/TankType.cs
@@ -54,6 +54,12 @@ public abstract class TankType : MonoBehaviour
         if (this is Level4Tank) return 5;
         if (this is Level4377Tank) return 6;
         if (this is LevelEasterTank) return 3;
+        if (this is Level5Tank) return 3;
+        if (this is Level6Tank) return 3;
+        if (this is Level7Tank) return 3;
+        if (this is Level8Tank) return 3;
+        if (this is Level9Tank1) return 3;
+        if (this is Level10Tank1) return 3;
         return 1; // Default fallback
     }
     public int GetDamage()
