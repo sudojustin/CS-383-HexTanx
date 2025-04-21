58522f18bd9c0580a743f22a0c05a93a187b402d
Brendomcwumbo <148289995+Brendomcwumbo@users.noreply.github.com>
2025-04-21 03:47:20 -0700

    Commitasdasd
    Remove The new stuff in the place tile script to undo.

--- Assets/src/TL2_Malik/Assets/PlaceTile.cs
+++ Assets/src/TL2_Malik/Assets/PlaceTile.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 
 public class PlaceTile : MonoBehaviour
 {
@@ -128,4 +128,157 @@ public class PlaceTile : MonoBehaviour
                 }
     }
     
+}*/
+using UnityEngine;
+
+public class PlaceTile : MonoBehaviour
+{
+    public Texture2D layoutTexture;
+    public bool[,] mapMask;
+    public bool runOnStart = true;
+
+    private int choseTerrain = 0;
+    private int isTerrain = 0;
+    private float hexwidth = 1f;
+    public float hexHeight = Mathf.Sqrt(3) / 2f;
+    public float heightspacing;
+    public int width;
+    public int height;
+    public float ypos;
+    public float xpos;
+    public float offset;
+    public float offset2;
+    public int min;
+    public int max;
+
+    public GameObject Tile;
+    public GameObject Terrain1; // Fire
+    public GameObject Terrain2; // Earth
+    public GameObject Terrain3; // Ice
+    public Vector3[,] Grid;
+
+    [SerializeField]
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+        if (cam == null)
+        {
+            Debug.LogError("Main camera is not assigned or not found!");
+            return;
+        }
+
+        if (layoutTexture != null)
+        {
+            GenerateMapFromTexture();
+        }
+        else if (runOnStart)
+        {
+            height = Random.Range(min, max);
+            width = Random.Range(min, max);
+            MakeMap(width, height);
+        }
+    }
+
+    public void GenerateMapFromTexture()
+    {
+        width = layoutTexture.width;
+        height = layoutTexture.height;
+        Grid = new Vector3[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color pixel = layoutTexture.GetPixel(x, y);
+                if (pixel.a < 0.1f || pixel == Color.black) continue;
+
+                Vector3 tilePos = CalculateTilePosition(x, y);
+                Grid[x, y] = tilePos;
+
+                if (pixel == Color.white)
+                    Instantiate(Tile, tilePos, Quaternion.identity);
+                else if (pixel == Color.red)
+                    Instantiate(Terrain1, tilePos, Quaternion.identity); // Fire
+                else if (pixel == Color.green)
+                    Instantiate(Terrain2, tilePos, Quaternion.identity); // Earth
+                else if (pixel == Color.blue)
+                    Instantiate(Terrain3, tilePos, Quaternion.identity); // Ice
+                else
+                    Instantiate(Tile, tilePos, Quaternion.identity); // default fallback
+            }
+        }
+
+        cam.transform.position = new Vector3(width / 2f, height / 2f, -15);
+    }
+
+    Vector3 CalculateTilePosition(int x, int y)
+    {
+        float xpos = x * hexwidth;
+        float ypos = y * hexHeight;
+        if (y % 2 == 1) xpos += offset;
+        return new Vector3(xpos + hexwidth / 2f, ypos, 0);
+    }
+
+    public void MakeMap(int width, int height)
+    {
+        Grid = new Vector3[width, height];
+
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                if (mapMask != null && !mapMask[x, y])
+                    continue;
+                TileFactory(x, y);
+            }
+        }
+
+        if (cam != null)
+        {
+            cam.transform.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -15);
+        }
+        else
+        {
+            Debug.LogError("Camera (cam) is not assigned!");
+        }
+    }
+
+    void TileFactory(int x, int y)
+    {
+        isTerrain = Random.Range(1, 3);
+        choseTerrain = Random.Range(1, 4);
+        xpos = x * hexwidth;
+        ypos = y * hexHeight * 1.0f;
+
+        if (y % 2 == 1)
+        {
+            xpos += offset;
+        }
+        if (isTerrain == 2)
+        {
+            if (choseTerrain == 1)
+            {
+                Grid[x, y] = new Vector3(xpos += hexwidth / 2f, ypos, 0);
+                Instantiate(Terrain2, Grid[x, y], Quaternion.identity);
+            }
+            else if (choseTerrain == 2)
+            {
+                Grid[x, y] = new Vector3(xpos += hexwidth / 2f, ypos, 0);
+                Instantiate(Terrain3, Grid[x, y], Quaternion.identity);
+            }
+            else
+            {
+                Grid[x, y] = new Vector3(xpos += hexwidth / 2f, ypos, 0);
+                Instantiate(Terrain1, Grid[x, y], Quaternion.identity);
+            }
+        }
+        else
+        {
+            Grid[x, y] = new Vector3(xpos += hexwidth / 2f, ypos, 0);
+            Instantiate(Tile, Grid[x, y], Quaternion.identity);
+        }
+    }
 }
+
